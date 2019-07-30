15cf967eb24a722edfc510451c8120d12ba270ff
mehedihasan <mehedihasan552827@gmail.com>
2019-07-31 00:22:45 +0600

    all method removed and add messege() method

--- Hello.java
+++ Hello.java
@@ -2,19 +2,10 @@ public class Hello{
    public static void main(String[] args){
       System.otu.println("vs code is awesome.");
 
-      display();
-      welcome();
    }
 
-   public static void display(){
-      System.out.println("bye bye");
+   public static void message(){
+      System.out.println("hooray!!! now we are able to contribute on open source");
    }
 
-   public static int sqr(int x){
-      return x*x;
-   }
-   
-   public static void welcome(){
-      System.out.println("hello i have to go");
-   }
 }
