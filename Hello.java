919bee60c5d6f38fa866de9c31794df19692ed00
mehedihasan <mehedihasan552827@gmail.com>
2019-07-30 16:21:36 +0600

    added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
    public static void main(String[] args){
       System.otu.println("vs code is awesome.");
+
+      display("mehedi hasan");
+   }
+
+   public static void display(String name){
+      System.out.println("welcome" +name);
    }
 }
