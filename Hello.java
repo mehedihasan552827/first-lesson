fe6c6954c8af7cc92e71513f742fdbce0c12c28a
mehedihasan <mehedihasan552827@gmail.com>
2019-07-30 18:10:03 +0600

    remove greeitng and add display method

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello{
    public static void main(String[] args){
       System.otu.println("vs code is awesome.");
 
-      display("git");
+      display();
    }
 
-   public static void display(String name){
-      System.out.println("bye bye" +name);
+   public static void display(){
+      System.out.println("bye bye");
    }
 }
