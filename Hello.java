a0bc0c06e26c65e29e9bf2dcff7e74e5ead4ddcd
mehedihasan <mehedihasan552827@gmail.com>
2019-07-30 16:46:25 +0600

    change greeting method

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello{
    public static void main(String[] args){
       System.otu.println("vs code is awesome.");
 
-      display("mehedi hasan");
+      display("git");
    }
 
    public static void display(String name){
-      System.out.println("welcome" +name);
+      System.out.println("bye bye" +name);
    }
 }
