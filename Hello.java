0a03d06f2a1c79a6b68a18db17fa7c73c15b4aef
mehedihasan <mehedihasan552827@gmail.com>
2019-07-30 18:37:26 +0600

    change greeting to welcome

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
       System.otu.println("vs code is awesome.");
 
       display();
-      greeting();
+      welcome();
    }
 
    public static void display(){
@@ -11,7 +11,7 @@ public class Hello{
    }
 
    
-   public static void greeting(){
-      System.out.println("goodby bye");
+   public static void welcome(){
+      System.out.println("hello i have to go");
    }
 }
